4116182374fdebbeefeedf633840cd5b52a4c205
sayTG <busarithienkie@gmail.com>
2022-06-09 23:37:09 +0100

    feat: allow json import

--- Compliant-Frontend/src/app/app.component.ts
+++ Compliant-Frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import  configurl from 'src/assets/config.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  base = environment.BASE_URL;
+  //base = environment.BASE_URL;
+  base = configurl.apiServer.url;
   requests = environment.REQUESTS;
 }
