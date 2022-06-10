20c5512691f163074b7f62996ff1dedb125a479c
sayTG <busarithienkie@gmail.com>
2022-06-10 03:37:19 +0100

    feat: added nginx and frontend docker file

--- Compliant-Frontend/src/environments/environment.prod.ts
+++ Compliant-Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,23 @@
 export const environment = {
   production: true,
 //Production
+
+BASE_URL: 'https://localhost:44396',
+  REQUESTS: [
+    {
+      verb: 'get',
+      url: '/chuck/categories',
+    },
+    {
+      verb: 'get',
+      url:  '/search',
+      queries: 'query=search',
+    },
+    {
+      verb: 'get',
+      url: '/swapi/people',
+      queries: 'pages=1',
+    },
+  ],
+  
 };
