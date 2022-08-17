84c858aeb9dc8f2e526671a1e0e79ff72167aea2
sayTG <busarithienkie@gmail.com>
2022-08-17 23:40:24 +0100

    feat: added not found response

---
+++ CompliantAPI/Utilities/Reponses/ApiNotFoundResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CompliantAPI.Utilities.Reponses
+{
+    public sealed class ApiNotFoundResponse : ApiBaseResponse
+    {
+        public string Message { get; set; }
+        public ApiNotFoundResponse(string message) : base(false)
+        {
+            Message = message;
+        }
+    }
+}
